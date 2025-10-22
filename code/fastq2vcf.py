1ffb5712691f3e1d942b5e9d1fe3f6d92bf312ec
Nina <78304174+NinaMercedes@users.noreply.github.com>
2025-10-22 13:14:34 +0100

    Update fastq2vcf.py

--- code/fastq2vcf.py
+++ code/fastq2vcf.py
@@ -1,13 +1,18 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
 import sys
 import argparse
+import os
 import fastq2matrix as fm
-import os.path
 
-
-def get_step_num(prefix):
-    files = {f"{prefix}.mkdup.bam.bai":1,f"{prefix}.bqsr.bam.bai":2,f"{prefix}.bqsr.cram.crai":2,f"{prefix}.mkdup.cram.crai":2,f"{prefix}.g.vcf.gz.validated":3}
+def get_step_num(prefix_path):
+    files = {
+        f"{prefix_path}.mkdup.bam.bai": 1,
+        f"{prefix_path}.bqsr.bam.bai": 2,
+        f"{prefix_path}.bqsr.cram.crai": 2,
+        f"{prefix_path}.mkdup.cram.crai": 2,
+        f"{prefix_path}.g.vcf.gz.validated": 3
+    }
     step = 0
     for f in files:
         if os.path.isfile(f):
@@ -15,129 +20,161 @@ def get_step_num(prefix):
             sys.stderr.write(f"Found {f}\n")
     return step
 
-
 def main_trim(args):
+    args.prefix_path = os.path.join(args.tmp_dir, args.prefix)
     if args.single:
-        fm.run_cmd("trimmomatic SE -phred33 %(read1)s %(prefix)s_trimmed.fq LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36" % vars(args))
+        trimmed_file = f"{args.prefix_path}_trimmed.fq"
+        fm.run_cmd(f"trimmomatic SE -phred33 {args.read1} {trimmed_file} LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36")
     else:
-        fm.run_cmd("trimmomatic PE -phred33 %(read1)s %(read2)s -baseout %(prefix)s LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36" % vars(args))
-
+        fm.run_cmd(f"trimmomatic PE -phred33 {args.read1} {args.read2} -baseout {args.prefix_path} LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36")
+    args.trimmed = True
 
 def main_map(args):
-    args.step = get_step_num(args.prefix)
+    args.prefix_path = os.path.join(args.tmp_dir, args.prefix)
+    args.step = get_step_num(args.prefix_path)
+
+    # Determine which reads to map
     if getattr(args, "trimmed", False):
         if args.single:
-            args.reads = f"{args.prefix}_trimmed.fq"
+            reads = f"{args.prefix_path}_trimmed.fq"
         else:
-            args.reads = f"{args.prefix}_1P {args.prefix}_2P"
+            reads = f"{args.prefix_path}_1P {args.prefix_path}_2P"
     else:
-        # No trimming; use original input reads
         if args.single:
-            args.reads = args.read1
+            reads = args.read1
         else:
-            args.reads = f"{args.read1} {args.read2}"
-    if args.redo or args.step<1:
-        fm.run_cmd("bwa mem -t %(threads)s -R \"@RG\\tID:%(prefix)s\\tSM:%(prefix)s\\tPL:Illumina\" %(ref)s %(reads)s | samtools view -@ %(threads)s -b - | samtools fixmate -@ %(threads)s -m - - | samtools sort -@ %(threads)s - | samtools markdup -@ %(threads)s - %(prefix)s.mkdup.bam -" % vars(args))
-    if getattr(args, "trimmed", False) and args.single:
-        fm.run_cmd(f"rm -f {args.reads}")
-    if getattr(args, "trimmed", False) and not args.single:
-        fm.run_cmd(f"rm -f {args.prefix}_1P {args.prefix}_2P {args.prefix}_1U {args.prefix}_2U")
-        fm.run_cmd("samtools index -@ %(threads)s %(prefix)s.mkdup.bam" % vars(args))
-        fm.run_cmd("samtools flagstat -@ %(threads)s %(prefix)s.mkdup.bam > %(prefix)s.mkdup.bamstats" % vars(args))
-    if args.bqsr_vcf and (args.redo or args.step<2):
+            reads = f"{args.read1} {args.read2}"
+
+    drop_unmapped = "-F 4" if getattr(args, "drop_unmapped", False) else ""
+
+    # Mapping command
+    fm.run_cmd(f"{args.mapper} mem -t {args.threads} -R \"@RG\\tID:{args.prefix}\\tSM:{args.prefix}\\tPL:Illumina\" {args.ref} {reads} | "
+               f"samtools view -@ {args.threads} {drop_unmapped} -b - | "
+               f"samtools fixmate -@ {args.threads} -m - - | "
+               f"samtools sort -@ {args.threads} - | "
+               f"samtools markdup -@ {args.threads} - {args.prefix_path}.mkdup.bam -")
+
+    # Remove trimmed files
+    if getattr(args, "trimmed", False):
+        if args.single:
+            fm.run_cmd(f"rm -f {reads}")
+        else:
+            fm.run_cmd(f"rm -f {args.prefix_path}_1P {args.prefix_path}_2P {args.prefix_path}_1U {args.prefix_path}_2U")
+
+    # Index and flagstat
+    fm.run_cmd(f"samtools index -@ {args.threads} {args.prefix_path}.mkdup.bam")
+    fm.run_cmd(f"samtools flagstat -@ {args.threads} {args.prefix_path}.mkdup.bam > {args.prefix_path}.mkdup.bamstats")
+
+    # BQSR
+    if args.bqsr_vcf and (args.redo or args.step < 2):
         for vcf in args.bqsr_vcf.split(","):
             fm.tabix_vcf(vcf)
-        args.bqsr_vcf = " ".join(["--known-sites %s" % s for s in args.bqsr_vcf.split(",")])
-        fm.run_cmd("gatk BaseRecalibrator -R %(ref)s -I %(prefix)s.mkdup.bam %(bqsr_vcf)s -O %(prefix)s.recal_data.table" % vars(args))
-        fm.run_cmd("gatk ApplyBQSR -R %(ref)s -I %(prefix)s.mkdup.bam --bqsr-recal-file %(prefix)s.recal_data.table -O %(prefix)s.bqsr.bam" % vars(args))
-        fm.run_cmd("samtools index -@ %(threads)s %(prefix)s.bqsr.bam" % vars(args))
-        fm.run_cmd("samtools flagstat -@ %(threads)s %(prefix)s.bqsr.bam > %(prefix)s.bqsr.bamstats" % vars(args))
-        fm.run_cmd("rm %(prefix)s.mkdup.bam*" % vars(args))
-
+        bqsr_sites = " ".join([f"--known-sites {s}" for s in args.bqsr_vcf.split(",")])
+        fm.run_cmd(f"gatk BaseRecalibrator -R {args.ref} -I {args.prefix_path}.mkdup.bam {bqsr_sites} -O {args.prefix_path}.recal_data.table")
+        fm.run_cmd(f"gatk ApplyBQSR -R {args.ref} -I {args.prefix_path}.mkdup.bam --bqsr-recal-file {args.prefix_path}.recal_data.table -O {args.prefix_path}.bqsr.bam")
+        fm.run_cmd(f"samtools index -@ {args.threads} {args.prefix_path}.bqsr.bam")
+        fm.run_cmd(f"samtools flagstat -@ {args.threads} {args.prefix_path}.bqsr.bam > {args.prefix_path}.bqsr.bamstats")
+        fm.run_cmd(f"rm {args.prefix_path}.mkdup.bam*")
+        args.bam = f"{args.prefix_path}.bqsr.bam"
+    else:
+        args.bam = f"{args.prefix_path}.mkdup.bam"
 
 def main_gatk(args):
     if not args.prefix:
-        args.prefix = args.bam.replace(".bam","")
-    fm.run_cmd("gatk HaplotypeCaller -I %(bam)s -R %(ref)s -O %(prefix)s.g.vcf.gz -ERC %(erc)s" % vars(args))
-    fm.run_cmd("gatk ValidateVariants -V %(prefix)s.g.vcf.gz -gvcf -R %(ref)s && touch %(prefix)s.g.vcf.gz.validated" % vars(args))
+        args.prefix = args.bam.replace(".bam", "")
+    fm.run_cmd(f"gatk HaplotypeCaller -I {args.bam} -R {args.ref} -O {args.prefix}.g.vcf.gz -ERC {args.erc}")
+    fm.run_cmd(f"gatk ValidateVariants -V {args.prefix}.g.vcf.gz -gvcf -R {args.ref} && touch {args.prefix}.g.vcf.gz.validated")
 
-def convert_to_cram(bam_file,ref_file,threads):
-    cram_file = bam_file.replace(".bam",".cram")
-    fm.run_cmd("samtools view -@ %s -C %s -o %s -T %s" %(threads,bam_file,cram_file,ref_file))
-    fm.run_cmd("samtools index %s" % cram_file)
-    fm.run_cmd("rm %s %s.bai" % (bam_file,bam_file))
+def convert_to_cram(bam_file, ref_file, threads):
+    cram_file = bam_file.replace(".bam", ".cram")
+    fm.run_cmd(f"samtools view -@ {threads} -C {bam_file} -o {cram_file} -T {ref_file}")
+    fm.run_cmd(f"samtools index {cram_file}")
+    fm.run_cmd(f"rm {bam_file} {bam_file}.bai")
+    return cram_file
 
 def main_all(args):
+    args.prefix_path = os.path.join(args.tmp_dir, args.prefix)
     fm.create_seq_dict(args.ref)
-    fm.bwa_index(args.ref)
+    if args.mapper == "bwa":
+        fm.bwa_index(args.ref)
+    elif args.mapper == "bwa-mem2":
+        fm.bwa2_index(args.ref)
     fm.faidx(args.ref)
 
-    args.step = get_step_num(args.prefix)
+    args.step = get_step_num(args.prefix_path)
 
-    if not args.read2 and not args.single:
-        sys.stderr.write("Second read is not provided, please check... Exiting!\n")
-        quit()
+    if not args.single and not args.read2:
+        sys.stderr.write("Second read is missing for paired-end run. Exiting.\n")
+        sys.exit(1)
 
-    args.bam = args.prefix+".bqsr.bam" if args.bqsr_vcf else args.prefix+".mkdup.bam"
-
-    sys.stderr.write("Starting at step %s\n" % (args.step+1))
+    sys.stderr.write(f"Starting at step {args.step+1}\n")
     if args.trim and (args.redo or args.step < 1):
         sys.stderr.write("Running trimming step...\n")
         main_trim(args)
-        args.trimmed = True
     else:
         sys.stderr.write("Skipping trimming step (use --trim to enable)\n")
         args.trimmed = False
-    if args.redo or args.step<2:
-        main_map(args)
-    if args.redo or args.step<3:
-        sys.stderr.write("Using %(bam)s as the bam file" % vars(args))
-        main_gatk(args)
-    if args.cram:
-        if args.redo or args.step<4:
-            convert_to_cram(args.bam,args.ref,args.threads)
 
-parser = argparse.ArgumentParser(description='fastq2matrix pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    main_map(args)
+
+    if args.redo or args.step < 3:
+        sys.stderr.write(f"Using {args.bam} as the bam file\n")
+        main_gatk(args)
+
+    if args.cram:
+        if args.redo or args.step < 4:
+            args.bam = convert_to_cram(args.bam, args.ref, args.threads)
+
+# ---------------- Argument parsing ----------------
+parser = argparse.ArgumentParser(description='fastq2matrix pipeline', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 subparsers = parser.add_subparsers(help="Task to perform")
 
-parser_sub = subparsers.add_parser('all', help='Trim reads using trimmomatic', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser_sub.add_argument('--read1','-1',help='First read file',required=True)
-parser_sub.add_argument('--read2','-2',help='Second read file')
-parser_sub.add_argument('--prefix','-p',help='Sample prefix for all results generated',required=True)
-parser_sub.add_argument('--ref','-r',help='Second read file',required=True)
-parser_sub.add_argument('--threads','-t',default=4,help='Number of threads')
-parser_sub.add_argument('--bqsr-vcf','-q',help='VCF file used for bqsr')
-parser_sub.add_argument('--erc',default="GVCF", choices=["GVCF","BP_RESOLUTION"], help='Choose ERC type on GATK')
-parser_sub.add_argument('--redo',action="store_true",help='Redo everything')
-parser_sub.add_argument('--single',action="store_true",help='Redo everything')
-parser_sub.add_argument('--cram',action="store_true",help='Redo everything')
-parser_sub.add_argument('--trim', action='store_true', help='Run trimming step using Trimmomatic before mapping')
+# All
+parser_sub = subparsers.add_parser('all', help='Run full pipeline', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser_sub.add_argument('--read1','-1', help='First read file', required=True)
+parser_sub.add_argument('--read2','-2', help='Second read file')
+parser_sub.add_argument('--prefix','-p', help='Sample prefix', required=True)
+parser_sub.add_argument('--ref','-r', help='Reference genome', required=True)
+parser_sub.add_argument('--threads','-t', default=4, type=int, help='Number of threads')
+parser_sub.add_argument('--bqsr-vcf','-q', help='VCF file used for BQSR')
+parser_sub.add_argument('--erc', default="GVCF", choices=["GVCF","BP_RESOLUTION"], help='ERC type for GATK')
+parser_sub.add_argument('--redo', action="store_true", help='Redo all steps')
+parser_sub.add_argument('--single', action="store_true", help='Single-end reads')
+parser_sub.add_argument('--cram', action="store_true", help='Convert BAM to CRAM')
+parser_sub.add_argument('--trim', action='store_true', help='Run trimming before mapping')
+parser_sub.add_argument('--mapper','-m', default="bwa", choices=["bwa","bwa-mem2"], help='Mapper to use')
+parser_sub.add_argument('--tmp-dir', default=".", help='Temporary working directory')
 parser_sub.set_defaults(func=main_all)
 
-parser_sub = subparsers.add_parser('trim', help='Trim reads using trimmomatic', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser_sub.add_argument('--prefix','-p',help='Sample prefix for all results generated',required=True)
+# Trim
+parser_sub = subparsers.add_parser('trim', help='Trim reads', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser_sub.add_argument('--prefix','-p', help='Sample prefix', required=True)
 parser_sub.set_defaults(func=main_trim)
 
-parser_sub = subparsers.add_parser('map', help='Trim reads using trimmomatic', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser_sub.add_argument('--read1','-1',help='First read file',required=True)
-parser_sub.add_argument('--read2','-2',help='Second read file',required=True)
-parser_sub.add_argument('--prefix','-p',help='Sample prefix for all results generated',required=True)
-parser_sub.add_argument('--ref','-r',help='Second read file',required=True)
-parser_sub.add_argument('--threads','-t',default=4,help='Number of threads')
-parser_sub.add_argument('--bqsr-vcf','-q',help='VCF file used for bqsr')
-parser_sub.add_argument('--redo',action="store_true",help='Redo everything')
+# Map
+parser_sub = subparsers.add_parser('map', help='Map reads', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser_sub.add_argument('--read1','-1', help='First read file', required=True)
+parser_sub.add_argument('--read2','-2', help='Second read file', required=True)
+parser_sub.add_argument('--prefix','-p', help='Sample prefix', required=True)
+parser_sub.add_argument('--ref','-r', help='Reference genome', required=True)
+parser_sub.add_argument('--threads','-t', default=4, type=int, help='Number of threads')
+parser_sub.add_argument('--bqsr-vcf','-q', help='VCF file used for BQSR')
+parser_sub.add_argument('--redo', action="store_true", help='Redo all steps')
+parser_sub.add_argument('--single', action="store_true", help='Single-end reads')
+parser_sub.add_argument('--cram', action="store_true", help='Convert BAM to CRAM')
+parser_sub.add_argument('--mapper','-m', default="bwa", choices=["bwa","bwa-mem2"], help='Mapper to use')
 parser_sub.set_defaults(func=main_map)
 
-parser_sub = subparsers.add_parser('gatk', help='Trim reads using trimmomatic', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser_sub.add_argument('--bam','-b',help='First read file',required=True)
-parser_sub.add_argument('--ref','-r',help='Second read file',required=True)
-parser_sub.add_argument('--prefix','-p',help='Sample prefix for all results generated')
-parser_sub.add_argument('--erc',default="GVCF", choices=["GVCF","BP_RESOLUTION"], help='Choose ERC type on GATK')
-parser_sub.add_argument('--threads','-t',default=4,help='Number of threads')
-
+# GATK
+parser_sub = subparsers.add_parser('gatk', help='Run GATK variant calling', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser_sub.add_argument('--bam','-b', help='Input BAM file', required=True)
+parser_sub.add_argument('--ref','-r', help='Reference genome', required=True)
+parser_sub.add_argument('--prefix','-p', help='Sample prefix')
+parser_sub.add_argument('--erc', default="GVCF", choices=["GVCF","BP_RESOLUTION"], help='ERC type for GATK')
+parser_sub.add_argument('--threads','-t', default=4, type=int, help='Number of threads')
 parser_sub.set_defaults(func=main_gatk)
 
-
+# ---------------- Parse args ----------------
 args = parser.parse_args()
 if vars(args) == {}:
     parser.print_help(sys.stderr)
